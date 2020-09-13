587fa08741a8d43540860345f20fcd1eb40565a1
Isaías França <isaias_leite_@outlook.com>
2020-09-13 13:21:21 -0300

    Added CRUD with MySQL
    A Simple CRUD was created with MySQL.

--- C# (Sharp) - CRUD with MySQL/SQLs/SQL - version 1.sql
+++ C# (Sharp) - CRUD with MySQL/SQLs/SQL - version 1.sql
@@ -3,9 +3,10 @@
 CREATE SCHEMA `crud` DEFAULT CHARACTER SET utf8 ;
 
 CREATE TABLE `crud`.`user` (
-  `id_user` INT NOT NULL,
+  `id_user` INT(11) NOT NULL AUTO_INCREMENT,
   `name_user` VARCHAR(100) NOT NULL,
-  `fone_user` INT NOT NULL,
+  `fone_user` VARCHAR(15) NOT NULL,
   PRIMARY KEY (`id_user`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
--- C# (Sharp) - CRUD with MySQL/Visual Studio/C_Sharp_CRUD_with_MySQL/Form1.cs
+++ C# (Sharp) - CRUD with MySQL/Visual Studio/C_Sharp_CRUD_with_MySQL/Form1.cs
@@ -7,14 +7,171 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace C_Sharp_CRUD_with_MySQL
 {
     public partial class Form1 : Form
     {
+        MySqlConnection mySqlConnection;
+        MySqlCommand mySqlCommand;
+        MySqlDataReader mySqlDataReader;
+        MySqlDataAdapter mySqlDataAdapter;
+        string sql;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mySqlConnection = new MySqlConnection("Server=localhost;Database=crud;Uid=root");
+               
+                sql = "INSERT INTO User (name_user, fone_user) VALUES (@name_user, @fone_user)";
+
+                mySqlCommand = new MySqlCommand(sql, mySqlConnection);
+                mySqlCommand.Parameters.AddWithValue("@name_user", txtName.Text);
+                mySqlCommand.Parameters.AddWithValue("@fone_user", mtbFone.Text.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ",""));
+
+                mySqlConnection.Open();
+
+                mySqlCommand.ExecuteNonQuery().ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Data Successfully Inserted");
+                mySqlConnection.Close();
+                mySqlConnection = null;
+                mySqlCommand = null;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mySqlConnection = new MySqlConnection("Server=localhost;Database=crud;Uid=root");
+                
+                sql = "UPDATE User SET name_user = @name_user, fone_user = @fone_user WHERE id_user = @id";
+
+                mySqlCommand = new MySqlCommand(sql, mySqlConnection);
+                mySqlCommand.Parameters.AddWithValue("@id", txtID.Text);
+                mySqlCommand.Parameters.AddWithValue("@name_user", txtName.Text);
+                mySqlCommand.Parameters.AddWithValue("@fone_user", mtbFone.Text.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", ""));
+
+                mySqlConnection.Open();
+
+                mySqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Data Successfully Edited");
+                mySqlConnection.Close();
+                mySqlConnection = null;
+                mySqlCommand = null;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mySqlConnection = new MySqlConnection("Server=localhost;Database=crud;Uid=root");
+                
+                sql = "DELETE FROM user WHERE id_user = @id";
+
+                mySqlCommand = new MySqlCommand(sql, mySqlConnection);
+                mySqlCommand.Parameters.AddWithValue("@id", txtID.Text);
+
+                mySqlConnection.Open();
+
+                mySqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Data Successfully Deleted");
+                mySqlConnection.Close();
+                mySqlConnection = null;
+                mySqlCommand = null;
+            }
+        }
+
+        private void btnConsult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mySqlConnection = new MySqlConnection("Server=localhost;Database=crud;Uid=root");
+
+                sql = "SELECT * FROM user WHERE id_user = @id";
+
+                mySqlCommand = new MySqlCommand(sql, mySqlConnection);
+                mySqlCommand.Parameters.AddWithValue("@id", txtID.Text);
+
+                mySqlConnection.Open();
+
+                mySqlDataReader = mySqlCommand.ExecuteReader();
+
+                while(mySqlDataReader.Read())
+                {
+                    txtName.Text = Convert.ToString(mySqlDataReader["name_user"]);
+                    mtbFone.Text = Convert.ToString(mySqlDataReader["fone_user"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Data Consulted Successfully");
+                mySqlConnection.Close();
+                mySqlConnection = null;
+                mySqlCommand = null;
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mySqlConnection = new MySqlConnection("Server=localhost;Database=crud;Uid=root");
+
+                sql = "SELECT * FROM user";
+
+                mySqlDataAdapter = new MySqlDataAdapter(sql, mySqlConnection);
+
+                DataTable dataTable = new DataTable();
+
+                mySqlDataAdapter.Fill(dataTable);
+
+                dgvDataBase.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Data Displayed Successfully");
+                mySqlConnection.Close();
+                mySqlConnection = null;
+                mySqlCommand = null;
+            }
         }
     }
 }
