c52f5b735a1e566d808c6e05fd1004d7c0f297ab
Isaías França <isaias_leite_@outlook.com>
2020-09-13 13:42:04 -0300

    Update SQL
    Update SQL.

--- C# (Sharp) - CRUD with MySQL/SQLs/SQL - version 1.sql
+++ C# (Sharp) - CRUD with MySQL/SQLs/SQL - version 1.sql
@@ -1,7 +1,9 @@
 -- SQL - Version 1
 
+-- Create Database 
 CREATE SCHEMA `crud` DEFAULT CHARACTER SET utf8 ;
 
+-- Create Table 
 CREATE TABLE `crud`.`user` (
   `id_user` INT(11) NOT NULL AUTO_INCREMENT,
   `name_user` VARCHAR(100) NOT NULL,
@@ -9,4 +11,7 @@ CREATE TABLE `crud`.`user` (
   PRIMARY KEY (`id_user`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
+-- Select
+SELECT * FROM `User`
 
